3a5cfe686903e811bfd26ae387688285818134c6
cuncinc <cuncinc@foxmail.com>
2021-05-17 19:15:49 +0800

    feat: HttpClient

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,49 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "httpclient.h"
+
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QJsonValue>
+
+void test()
+{
+	HttpClient("/cloudsearch").success([=](const QString &response) {
+		QJsonDocument jsonDoc = QJsonDocument::fromJson(response.toUtf8());
+		QJsonObject jsonObject = jsonDoc.object().value("result").toObject();
+		QJsonArray jsonArray = jsonObject.value("songs").toArray();
+
+		//		for (int i=0; i<jsonArray.size(); ++i)
+		//		{
+		//			QString name = jsonArray[i].toObject().value("name").toString();
+		//			qDebug() << name;
+		//		}
+
+		foreach (const QJsonValue& value, jsonArray)
+		{
+			int id = value.toObject().value("id").toInt();
+			QString name = value.toObject().value("name").toString();
+			qDebug() << id << "\t" << name;
+		}
+
+//		qDebug() << response;
+
+	}).param("keywords", "GoodTime")			// ²ÎÊý
+			.param("limit", "5")
+			.header("token", "test")		// Header
+			.get();
+}
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+	: QMainWindow(parent)
+	, ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
+	test();
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+	delete ui;
 }
-
